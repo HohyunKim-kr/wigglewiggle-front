c6f3190b0f7dd8b745847231feaa2603ccf35db3
hyunleee <105576592+hyunleee@users.noreply.github.com>
2023-12-02 18:33:13 +0900

    Feat: add cat image

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,6 +1,11 @@
 "use client";
+import Image from "next/image";
 const Editor = () => {
-  return <div>에디터</div>;
+  return (
+    <div>
+      <Image src="/cat.jpeg" alt="cat" width={100} height={100} />
+    </div>
+  );
 };
 
 export default Editor;
