d776b3f201883d042fb8156298870174f5fb2d77
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-12-29 17:14:48 +0900

    Add: connect abis

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,6 +1,30 @@
 "use client";
+import { AlchemyProvider, Contract, ethers } from "ethers";
 import { styled } from "styled-components";
+import ABI from "../../abis/WiggleFree.json";
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProviderState, getSignerState } from "@/redux/slice/web3Slice";
+
+const wiggleFreeAddress = "0x56a7753bd4b1DD1715Ef428AaDD8d4494ad0b0a8";
+
 const Game = () => {
+  const signer = useSelector(getSignerState);
+  const provider = useSelector(getProviderState);
+  console.log(provider);
+  const wiggleFreeContract = new ethers.Contract(
+    wiggleFreeAddress,
+    ABI,
+    provider
+  );
+
+  const somefunction = async () => {
+    const transaction_getUserList = await wiggleFreeContract.getUserList();
+    console.log(transaction_getUserList);
+  };
+
+  somefunction();
+
   return <Container></Container>;
 };
 
--- src/components/W3AWallet.tsx
+++ src/components/W3AWallet.tsx
@@ -8,9 +8,14 @@ import { Web3Auth, Web3AuthOptions } from "@web3auth/modal";
 import YellowShortButton from "./YellowShortButton";
 import { useEffect, useState } from "react";
 import YellowLongButton from "./YellowLongButton";
-import Web3 from "web3";
+import Web3, { Contract } from "web3";
 import { useDispatch } from "react-redux";
 import { SET_USER_LOGIN } from "@/redux/slice/authSlice";
+import { SET_PROVIDER_SIGNER } from "@/redux/slice/web3Slice";
+import { AlchemyProvider, ethers } from "ethers";
+import ABI from ".././abis/WiggleFree.json";
+
+// const wiggleFreeAddress = "0x737C374a0c2cb6CeFa7D9f33b768Af4b9985e57e";
 
 const W3AWallet = () => {
   const Web3AuthOptions: Web3AuthOptions = {
@@ -18,8 +23,10 @@ const W3AWallet = () => {
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET, // import {WEB3AUTH_NETWORK} from "@web3auth/base";
     chainConfig: {
       chainNamespace: CHAIN_NAMESPACES.EIP155,
-      chainId: "0x1",
-      rpcTarget: "https://rpc.ankr.com/eth", // This is the mainnet RPC we have added, please pass on your own endpoint while creating an app
+      chainId: "0x13881",
+      // rpcTarget: process.env.NEXT_PUBLIC_RPC_URL,
+      rpcTarget: "https://rpc.ankr.com/polygon_mumbai",
+      displayName: "Polygon Mumbai Testnet",
     },
     uiConfig: {
       appName: "Wiggle Wiggle",
@@ -33,10 +40,10 @@ const W3AWallet = () => {
       modalZIndex: "99998",
     },
   };
-
+  const dispatch = useDispatch();
   const web3auth = new Web3Auth(Web3AuthOptions);
   const [web3, setWeb3] = useState<Web3 | null>(null);
-  const dispatch = useDispatch();
+
   useEffect(() => {
     async function initialize() {
       await web3auth.initModal();
@@ -71,10 +78,35 @@ const W3AWallet = () => {
         const web3 = new Web3(web3authProvider!);
         setWeb3(web3);
 
+        console.log(process.env.NEXT_PUBLIC_API_KEY);
+
+        const provider = new AlchemyProvider(
+          "maticmum",
+          process.env.NEXT_PUBLIC_API_KEY
+        );
+
         const userInfo = await web3auth.getUserInfo();
         console.log(userInfo);
+
         const address = (await web3!.eth.getAccounts())[0];
         console.log(address);
+
+        const privateKey = await web3auth.provider!.request({
+          method: "private_key",
+        });
+
+        if (typeof privateKey === "string") {
+          const signer = new ethers.Wallet(privateKey, provider);
+          dispatch(SET_PROVIDER_SIGNER({ provider: provider, signer: signer }));
+          // const wiggleFreeContract = new ethers.Contract(
+          //   wiggleFreeAddress,
+          //   ABI,
+          //   signer
+          // );
+          // const transaction_getUserList =
+          //   await wiggleFreeContract.getUserList();
+          // console.log(transaction_getUserList);
+        }
         dispatch(
           SET_USER_LOGIN({
             address: address,
