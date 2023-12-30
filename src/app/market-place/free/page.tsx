ef0b4bbf07c8cb7b89adb8595ef0ee2c6e7009a0
hyunleee <105576592+hyunleee@users.noreply.github.com>
2023-12-30 16:46:40 +0900

    Change: market-place-design-change

--- src/app/market-place/free/page.tsx
+++ src/app/market-place/free/page.tsx
@@ -1,85 +1,189 @@
 "use client";
 
+import colors from "@/styles/color";
 import { styled } from "styled-components";
+import { usePathname, useRouter } from "next/navigation";
 
-const FreeDetail = () => {
+const MarketPlaceFree = () => {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <Container>
-      <Item style={{ marginTop: "30px" }}>Free</Item>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat </Name>
-        </Cardname>
-      </Card>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat </Name>
-        </Cardname>
-      </Card>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat</Name>
-        </Cardname>
-      </Card>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat </Name>
-        </Cardname>
-      </Card>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat </Name>
-        </Cardname>
-      </Card>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat </Name>
-        </Cardname>
-      </Card>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat </Name>
-        </Cardname>
-      </Card>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat </Name>
-        </Cardname>
-      </Card>
-      <Card>
-        <Img src="/images/cat.jpeg" alt="item1" />
-        <Cardname>
-          <Name>Cat </Name>
-        </Cardname>
-      </Card>
+      <Header>
+        <FreeOrPremium
+          onClick={() => router.push("/market-place/free")}
+          $isClicked={pathname === "/market-place/free"}
+        >
+          Free
+        </FreeOrPremium>
+        <Item style={{ fontSize: "50px", margin: "0" }}>|</Item>
+        <FreeOrPremium
+          onClick={() => router.push("/market-place/premium")}
+          $isClicked={pathname === "/market-place/premium"}
+        >
+          Premium
+        </FreeOrPremium>
+      </Header>
+      <MyProperty>
+        <MyCharacterContainer>
+          <Title style={{ marginTop: "0px" }}>
+            <Item>Character</Item>
+          </Title>
+          <MyCharacter style={{ marginLeft: "40px" }}>
+            <Character>
+              <CharacterImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Character>
+            <Character>
+              <CharacterImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Character>
+            <Character>
+              <CharacterImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Character>
+            <Character>
+              <CharacterImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Character>
+            <Character>
+              <CharacterImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Character>
+          </MyCharacter>
+        </MyCharacterContainer>
+
+        <MyClothesContainer>
+          <Title style={{ marginTop: "0px" }}>
+            <Item>Clothes</Item>
+          </Title>
+          <MyClothes style={{ marginLeft: "40px" }}>
+            <Clothes>
+              <ClothesImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Clothes>
+            <Clothes>
+              <ClothesImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Clothes>
+            <Clothes>
+              <ClothesImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Clothes>
+            <Clothes>
+              <ClothesImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Clothes>
+            <Clothes>
+              <ClothesImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Clothes>
+            <Clothes>
+              <ClothesImg src="/images/cat.jpeg" alt="item1" />
+              <Name>Cat </Name>
+              <Price>Free </Price>
+            </Clothes>
+          </MyClothes>
+        </MyClothesContainer>
+      </MyProperty>
     </Container>
   );
 };
 
-export default FreeDetail;
+export default MarketPlaceFree;
 
 const Container = styled.div`
   height: calc(100vh - 100px);
-  overflow-x: overlay;
+  display: flex;
+  flex-direction: column;
+`;
+const Header = styled.div`
+  width: fit-content;
+  height: fit-content;
+  padding: 30px;
+  padding-bottom: 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+const FreeOrPremium = styled.button<{ $isClicked: boolean }>`
+  font-size: 45px;
+  color: #dbd8cf;
+  height: fit-content;
+  width: fit-content;
+  border: 0;
+  background: transparent;
+  padding: 0 19px;
+  float: left;
+  color: ${(props) => (props.$isClicked ? colors.primary : colors.button)};
+  cursor: pointer;
+`;
+
+const MyProperty = styled.section`
+  display: flex;
+  flex-direction: row;
+`;
+const MyCharacterContainer = styled.div`
+  width: auto;
+  height: auto;
+`;
+const MyClothesContainer = styled.div`
+  width: auto;
+  height: auto;
+`;
+
+const MyCharacter = styled.div``;
+const MyClothes = styled.div``;
+
+const Title = styled.div`
+  width: 100%;
+  height: fit-content;
+  padding: 35px;
+  padding-bottom: 0px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
 `;
 
 const Item = styled.div`
-  color: #f8d49e;
-  font-size: 50px;
+  color: #d3d3d3;
+  font-size: 37px;
   line-height: 90%;
   margin-bottom: 25px;
   margin-left: 35px;
 `;
-const Card = styled.div`
-  height: 245px;
+
+const CharacterImg = styled.img`
+  width: 190px;
+  height: 255px;
+  border-radius: 18px;
+  margin-top: 19px;
+`;
+
+const ClothesImg = styled.img`
+  width: 130px;
+  height: 130px;
+  border-radius: 18px;
+  margin-top: 19px;
+`;
+
+const Name = styled.p`
+  color: #ffffff;
+  font-size: 30px;
+  margin-top: 19px;
+  margin-bottom: 10px;
+`;
+
+const Character = styled.div`
   border-radius: 18px;
   background: #292929;
   padding: 0 19px;
@@ -90,19 +194,20 @@ const Card = styled.div`
   float: left;
 `;
 
-const Name = styled.p`
-  color: #ffffff;
-  font-size: 30px;
-  margin-top: 19px;
-`;
-
-const Img = styled.img`
-  width: 235px;
-  height: 170px;
+const Clothes = styled.div`
   border-radius: 18px;
-  margin-top: 19px;
+  background: #292929;
+  padding: 0 19px;
+  width: fit-content;
+  height: fit-content;
+  margin-bottom: 25px;
+  margin-left: 40px;
+  float: left;
 `;
 
-const Cardname = styled.div`
-  height: 50px;
+const Price = styled.p`
+  color: #ffeed6;
+  font-size: 20px;
+  margin-top: 10px;
+  margin-bottom: 25px;
 `;
--- src/styles/color.ts
+++ src/styles/color.ts
@@ -2,6 +2,6 @@ const colors = {
   primary: "#F8D49E",
   white: "#FFFFFF",
   black: "000000",
+  button: "#DBD8CF",
 };
-
 export default colors;
