a5418dd4c0cd871b322490e415ea11616ceeb264
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-12-30 15:23:10 +0900

    Refactor: free character creation logic

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ethers } from "ethers";
+import { AlchemyProvider, ethers } from "ethers";
 import { styled } from "styled-components";
 import ABI from "../../abis/WiggleFree.json";
 import { useEffect, useMemo, useState } from "react";
@@ -7,38 +7,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProviderState, getSignerState } from "@/redux/slice/web3Slice";
 import { getAddressState } from "@/redux/slice/authSlice";
 import YellowLongButton from "@/components/YellowLongButton";
+import { useWeb3Auth } from "@/context/Web3AuthContext";
 
-const wiggleFreeAddress = "0x56a7753bd4b1DD1715Ef428AaDD8d4494ad0b0a8";
+const wiggleFreeAddress = "0x3d9E329f07eA03e792aA07B0025da568D4e608C4";
 
 const Game = () => {
-  const provider = useSelector(getProviderState);
+  const provider = new AlchemyProvider(
+    "maticmum",
+    process.env.NEXT_PUBLIC_API_KEY
+  );
   const signer = new ethers.Wallet( //contract 소유자가 직접 트랜잭션을 보내야함.
     process.env.NEXT_PUBLIC_PRIVATE_KEY!,
     provider
   );
 
   const userAddress = useSelector(getAddressState);
-  // userAddress.toUpperCase();
-  const wiggleFreeContract = new ethers.Contract(
-    wiggleFreeAddress,
-    ABI,
-    provider
-  );
 
-  const wiggleFreeContract1 = new ethers.Contract(
-    wiggleFreeAddress,
-    ABI,
-    signer
-  );
+  let wiggleFreeContract = new ethers.Contract(wiggleFreeAddress, ABI, signer);
 
   const [isFreeCharacterExist, setIsFreeCharacterExist] = useState(false);
 
   const checkFreeCharacterExistence = async () => {
+    wiggleFreeContract.connect(signer);
     // 사용자가 기본 캐릭터를 민팅받았는지 확인하는 함수
     const userList = await wiggleFreeContract.getUserList();
     for (let i = 0; i < userList.length; i++) {
       if (userAddress === userList[i]) {
         setIsFreeCharacterExist(true);
+        const tokenID = await wiggleFreeContract.getTokenIdByAddress(
+          userAddress
+        );
+        console.log(Number(tokenID));
         break;
       }
       setIsFreeCharacterExist(false);
@@ -47,9 +46,7 @@ const Game = () => {
 
   const createFreeCharacter = async () => {
     //기본 캐릭터를 생성하는 함수
-
-    // userAddress = ethers.getAddress(userAddress);
-    const mintNFT = await wiggleFreeContract1.safeMint(userAddress);
+    const mintNFT = await wiggleFreeContract.safeMint(userAddress);
     const receiptMintNFT = await mintNFT.wait();
     if (receiptMintNFT.status === 1) {
       //성공
