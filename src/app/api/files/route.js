9015627304487807770f9dc9b4113b16a5db0993
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2024-01-06 21:18:44 +0900

    Fix: ipfs image error

--- src/app/api/files/route.js
+++ src/app/api/files/route.js
@@ -14,22 +14,55 @@ export const dynamic = "force-dynamic"; // defaults to auto
 import { NextResponse } from "next/server";
 
 // To handle a POST request to /api
+// export async function POST(req) {
+//   try {
+//     const data = await req.json();
+//     const { filename, contentType, content } = data;
+
+//     // 임시 파일 경로 생성
+//     const tempPath = path.join(os.tmpdir(), filename);
+
+//     // Buffer를 임시 파일로 저장
+//     const buffer = Buffer.from(content, "base64");
+//     await writeFileAsync(tempPath, buffer);
+
+//     // 파일 스트림 생성
+//     const stream = createReadStream(tempPath);
+
+//     // IPFS에 업로드
+//     const options = {
+//       pinataMetadata: { name: filename },
+//     };
+//     const response = await pinata.pinFileToIPFS(stream, options);
+
+//     // 임시 파일 삭제
+//     await unlinkAsync(tempPath);
+
+//     return new Response(JSON.stringify({ ipfsHash: response.IpfsHash }), {
+//       status: 200,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return new Response("Server Error", { status: 500 });
+//   }
+// }
+
 export async function POST(req) {
   try {
     const data = await req.json();
     const { filename, contentType, content } = data;
 
+    // Base64 문자열을 Buffer로 변환
+    const buffer = Buffer.from(content, "base64");
     // 임시 파일 경로 생성
     const tempPath = path.join(os.tmpdir(), filename);
 
     // Buffer를 임시 파일로 저장
-    const buffer = Buffer.from(content, "base64");
     await writeFileAsync(tempPath, buffer);
 
-    // 파일 스트림 생성
+    // 파일 스트림 생성 및 IPFS에 업로드
     const stream = createReadStream(tempPath);
-
-    // IPFS에 업로드
     const options = {
       pinataMetadata: { name: filename },
     };
@@ -44,7 +77,6 @@ export async function POST(req) {
     });
   } catch (error) {
     console.error(error);
-    return new Response("Server Error", { status: 500 });
   }
 }
 
